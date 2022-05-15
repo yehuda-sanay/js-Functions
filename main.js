a994a548a1f8c4607ae561f50bc89297375516cb
yehuda-sanay <yehudasanay1@gmail.com>
2022-05-15 11:30:28 +0300

    13.05.22

---
+++ main.js
@@ -0,0 +1,16 @@
+// function printhelloworld(){
+//     var i=0
+//     while(i<3){
+//     document.write("hello world");
+//     ++i
+// }
+// }
+// printhelloworld()
+
+
+if(number1>number2){
+    var number1 = prompt(number1);
+    var number2 = prompt(number2);
+    document.write(number1);
+    return document.write(number2);
+}
